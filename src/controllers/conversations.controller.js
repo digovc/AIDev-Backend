0820223b6121124db04b59077a1d8de988bc0aa8
digovc <rodrigo.conspena@gmail.com>
2025-02-28 18:00:36 -0300

    Add endpoint to retrieve conversations by project ID in ConversationsService

--- src/controllers/conversations.controller.js
+++ src/controllers/conversations.controller.js
@@ -14,6 +14,16 @@ class ConversationsController extends CrudControllerBase {
     router.post(`/${ this.modelName }/:id/messages`, (req, res) => {
       this.createMessage(req, res).catch((e) => this.errorHandler(e, res));
     });
+
+    router.get(`/${ this.modelName }/by-project/:projectId`, (req, res) => {
+      this.getByProjectId(req, res).catch((e) => this.errorHandler(e, res));
+    });
+  }
+
+  async getByProjectId(req, res) {
+    const projectId = req.params.projectId;
+    const conversations = await this.service.findByProjectId(projectId);
+    res.json(conversations);
   }
 
   async createMessage(req, res) {
--- src/services/conversations.service.js
+++ src/services/conversations.service.js
@@ -19,6 +19,32 @@ class ConversationsService extends CrudServiceBase {
 
     await projectsService.update(project.id, project);
   }
+
+  async findByProjectId(projectId) {
+    // First, get the project
+    const project = await projectsService.getById(projectId);
+
+    if (!project) {
+      throw new Error('Project not found');
+    }
+
+    // If the project has no conversations, return empty array
+    if (!project.conversations || project.conversations.length === 0) {
+      return [];
+    }
+
+    // Get all conversations referenced in the project
+    const conversations = [];
+
+    for (const conversationId of project.conversations) {
+      const conversation = await this.getById(conversationId);
+      if (conversation) {
+        conversations.push(conversation);
+      }
+    }
+
+    return conversations;
+  }
 }
 
 module.exports = new ConversationsService();
