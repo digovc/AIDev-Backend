dceb755c7a44b91f054828de9180a24a7f894508
digovc <rodrigo.conspena@gmail.com>
2025-03-04 13:18:02 -0300

    Refactor router to use expressRouter and register messagesController endpoints

--- src/router.js
+++ src/router.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const projectsController = require('./controllers/projects.controller');
 const conversationsController = require("./controllers/conversations.controller");
+const messagesController = require("./controllers/messages.controller");
 const tasksController = require("./controllers/tasks.controller");
 
 class Router {
   constructor() {
-    this._router = express.Router();
+    this.expressRouter = express.Router();
 
     // Health check endpoint
-    this._router.get('/health-check', (req, res) => {
+    this.expressRouter.get('/health-check', (req, res) => {
       res.json({ status: 'ok', version: '1.0.0 alpha' });
     });
 
     // Register all controller endpoints
-    projectsController.registerEndpoints(this._router);
-    conversationsController.registerEndpoints(this._router);
-    tasksController.registerEndpoints(this._router);
+    projectsController.registerEndpoints(this.expressRouter);
+    conversationsController.registerEndpoints(this.expressRouter);
+    messagesController.registerEndpoints(this.expressRouter);
+    tasksController.registerEndpoints(this.expressRouter);
   }
 
   getRouter() {
-    return this._router;
+    return this.expressRouter;
   }
 }
 
