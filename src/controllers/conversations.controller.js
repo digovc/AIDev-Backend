9a5272dd589b635d4a611af692a4469a1bb87860
digovc <rodrigo.conspena@gmail.com>
2025-03-04 13:16:19 -0300

    Implement MessagesStore for message management and integrate with conversations

--- src/controllers/conversations.controller.js
+++ src/controllers/conversations.controller.js
@@ -1,5 +1,4 @@
 const CrudControllerBase = require('./crud-controller.base');
-const agentService = require('../services/agent.service');
 const conversationsStore = require('../stores/conversations.store');
 
 class ConversationsController extends CrudControllerBase {
@@ -10,38 +9,9 @@ class ConversationsController extends CrudControllerBase {
   registerEndpoints(router) {
     super.registerEndpoints(router);
 
-    router.post(`/${ this.modelName }/:id/messages`, (req, res) => {
-      this.createMessage(req, res).catch((e) => this.errorHandler(e, res));
-    });
-
     router.get(`/${ this.modelName }/project/:projectId`, (req, res) => {
       this.getByProjectId(req, res).catch((e) => this.errorHandler(e, res));
     });
-  }
-
-  async createMessage(req, res) {
-    const messageData = req.body;
-    const conversationId = req.params.id;
-    const conversation = await conversationsStore.getById(conversationId);
-
-    if (!conversation) {
-      throw new Error('Conversation not found');
-    }
-
-    if (!conversation.messages) {
-      conversation.messages = [];
-    }
-
-    messageData.createdAt = new Date().toISOString();
-    messageData.sender = 'user';
-
-    conversation.messages.push(messageData);
-
-    await conversationsStore.update(conversationId, conversation);
-
-    agentService.sendMessage(conversation).catch(console.error);
-
-    res.status(201).json(messageData);
   }
 
   async getByProjectId(req, res) {
--- src/stores/messages.store.js
+++ src/stores/messages.store.js
@@ -1,8 +1,50 @@
 const StoreBase = require('./store.base');
+const conversationsStore = require('./conversations.store');
+const socketIOService = require('../services/socket-io.service');
 
 class MessagesStore extends StoreBase {
   constructor() {
     super('messages', 'message');
+  }
+
+  async create(message) {
+    await super.create(message);
+    socketIOService.io.emit('message-created', message);
+    return message;
+  }
+
+  async prepareBeforeSave(message) {
+    await super.prepareBeforeSave(message);
+
+    const conversation = await conversationsStore.getById(message.conversationId);
+
+    if (!conversation) {
+      throw new Error('Conversation not found');
+    }
+
+    conversation.messages = conversation.messages || [];
+
+    if (!conversation.messages.includes(message.id)) {
+      conversation.messages.push(message.id);
+      await conversationsStore.update(conversation.id, conversation);
+    }
+  }
+
+  async getByConversationId(conversationId) {
+    const conversation = await conversationsStore.getById(conversationId);
+
+    if (!conversation) {
+      throw new Error('Conversation not found');
+    }
+
+    const messages = [];
+
+    for (const messageId of conversation.messages ?? []) {
+      const message = await this.getById(messageId);
+      messages.push(message);
+    }
+
+    return messages;
   }
 }
 
